9351b8de8b41cc31bd6e60df20ce4e2a53098681
github username <github email address>
2018-06-12 13:44:37 +0100

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON projects.id = pledges.project_id
   GROUP BY projects.title ORDER BY projects.title ASC;"
 end
@@ -17,7 +17,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
   FROM users
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON users.id = pledges.user_id
   GROUP BY users.name ORDER BY users.name ASC;"
 end
@@ -25,7 +25,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, (projects.funding_goal - SUM(pledges.amount))*-1 AS sub_value
   FROM projects
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON projects.id = pledges.project_id
   GROUP BY projects.title HAVING sub_value >= 0"
 end
@@ -33,7 +33,7 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
   "SELECT users.name, SUM(pledges.amount)
   FROM users
-  LEFT JOIN pledges
+  INNER JOIN pledges
   ON users.id = pledges.user_id
   GROUP BY users.name ORDER BY SUM(pledges.amount), users.name;"
 end
@@ -41,7 +41,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount
   FROM pledges
-  LEFT JOIN projects
+  INNER JOIN projects
   ON pledges.project_id = projects.id
   WHERE projects.category = 'music'"
 end
@@ -49,7 +49,7 @@ end
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
   "SELECT projects.category, SUM(pledges.amount)
   FROM pledges
-  LEFT JOIN projects
+  INNER JOIN projects
   ON pledges.project_id = projects.id
   WHERE projects.category = 'books'"
 end
